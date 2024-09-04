efc888df7948660a105d2aee7b8e5fdebe315d42
Bradley Onyett <bradley.onyett@gmail.com>
2024-09-04 15:05:51 +0100

    Add smooth scrolling to full layout

--- src/components/common/layouts/main.jsx
+++ src/components/common/layouts/main.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import Head from 'next/head';
 import { Box, Container } from '@chakra-ui/react';
 import NavBar from '../nav-bar/nav-bar';
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 function MainLayout({ children }) {
+  const MotionContainer = motion(Container);
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
     <Box as="main" display="inline-flex" height="100%" width="100%">
       <Head>
@@ -16,12 +25,12 @@ function MainLayout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet" />
       </Head>
 
-      <Container display="flex" flexDirection="column" p="0" m="0" maxW="100%" minH="100vh" overflow="auto">
+      <MotionContainer display="flex" flexDirection="column" p="0" m="0" maxW="100%" minH="100vh" overflow="auto">
         <NavBar />
 
         {children}
 
-      </Container>
+      </MotionContainer>
     </Box>
   );
 }

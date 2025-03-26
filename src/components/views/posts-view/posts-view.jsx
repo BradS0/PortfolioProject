d72879abb39c590b3937349b7ec50c55090889d0
BradS0 <bradley.onyett@gmail.com>
2025-03-26 11:37:27 +0000

    Fix build issue for vercel on unused page

--- src/components/views/posts-view/posts-view.jsx
+++ src/components/views/posts-view/posts-view.jsx
@@ -1,12 +1,12 @@
-// import React from 'react';
-// // import PageLayout from '../../common/layouts/page';
-// // import NavBar from '../../common/nav-bar/nav-bar';
+import { Text } from '@chakra-ui/react';
+import React from 'react';
+// import PageLayout from '../../common/layouts/page';
+// import NavBar from '../../common/nav-bar/nav-bar';
 
-// function PostsView() {
-//   return (
-//     <>
-//     </>
-//   );
-// }
+function PostsView() {
+  return (
+    <Text>Something will come of this page at some point :)</Text>
+  );
+}
 
-// export default PostsView;
+export default PostsView;
---
+++ src/pages/posts.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react';
+import PageLayout from '../components/common/layouts/page';
+import PostsView from '../components/views/posts-view/posts-view';
+
+function Posts() {
+  return (
+    <PageLayout title="Posts">
+      <PostsView />
+    </PageLayout>
+  );
+}
+
+export default Posts;
